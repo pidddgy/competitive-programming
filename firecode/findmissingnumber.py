a74cf86152dc14daf2d102297a30a1a33e2f2543
Marcus Chong <marcus.jn.chong@gmail.com>
2018-07-03 14:19:42 -0400

    did findmissingnumber from firecode

---
+++ firecode/findmissingnumber.py
@@ -0,0 +1,5 @@
+# Given an list containing 9 numbers ranging from 1 to 10, write a function to find the missing number. 
+# Assume you have 9 numbers between 1 to 10 and only one number is missing.
+
+def find_missing_number(list_numbers):
+  return(55 - sum(list_numbers))
